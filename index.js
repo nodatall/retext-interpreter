60f3f6cd5a69489c92b01b778d9260931a6cf4c6
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-01-22 16:08:32 -0800

    From the darkness come all fractals

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const retext = require('retext')
-const contractions = require('./retext-plugins/retext-contractions')
-const simplify = require('./retext-plugins/retext-simplify')
-const diacritics = require('./retext-plugins/retext-diacritics')
-const equality = require('./retext-plugins/retext-equality')
-const indefiniteArticle = require('./retext-plugins/retext-indefinite-article')
-const passive = require('./retext-plugins/retext-passive')
-const profanities = require('./retext-plugins/retext-profanities')
-const redundantAcronyms = require('./retext-plugins/retext-redundant-acronyms')
-const repeatedWords = require('./retext-plugins/retext-repeated-words')
-const overuse = require('./retext-plugins/retext-overuse')
-const usage = require('./retext-plugins/retext-usage')
-const readability = require('./retext-plugins/retext-readability')
-const spell = require('./retext-plugins/retext-spell')
+const contractions = require('retext-contractions')
+const simplify = require('retext-simplify')
+const diacritics = require('retext-diacritics')
+const equality = require('retext-equality')
+const indefiniteArticle = require('retext-indefinite-article')
+const passive = require('retext-passive')
+const profanities = require('retext-profanities')
+const redundantAcronyms = require('retext-redundant-acronyms')
+const repeatedWords = require('retext-repeated-words')
+const overuse = require('retext-overuse')
+const usage = require('retext-usage')
+const readability = require('retext-readability')
+const spell = require('retext-spell')
 const dictionary = require('dictionary-en-gb')
 const { send, json } = require('micro')
 
 const microCors = require('micro-cors')
+const cors = microCors({ allowMethods: ['PUT', 'POST'] })
 
 const proofRead = ( theString, callback ) => {
   let output
@@ -42,6 +43,8 @@ const proofRead = ( theString, callback ) => {
 
         messageContents.offending = message.offending
         messageContents.suggestion = message.suggestion
+        messageContents.location = message.location
+        messageContents.fatal = message.fatal
         messageContents.source = message.source
 
         return messageContents
@@ -49,35 +52,14 @@ const proofRead = ( theString, callback ) => {
 
       callback( result )
     })
-
 }
 
-const proofReadService = ( request, response ) => {
-  console.log(request)
-
-   return async function() {
-  //   console.log(Object.keys(request))
-    //console.log('request:', request.url, request.method)
+const proofReadService = async ( request, response ) => {
     const data = await json(request)
-    console.log("DATA", data);
-    send( response, 200, 'hello world 👏')
-  }
+
+    proofRead( data.content, ( result ) => {
+      send( response, 200, JSON.stringify(result))
+    })
 }
 
-const bigString = 'You can aggregate a shorter word. You does\'nt know me! Beyonce. His network was set up with a master and slave. He should, a 8-year old boy, should have arrived a hour. That movie was amazing. He was withheld while we were being fed. He’s pretty set on beating your butt for sheriff. Where can I find an ATM machine? Well, it it doesn\'t have to to be. The constellation also contains an isolated neutron star—Calvera—and H1504+65 the hottest white dwarf yet discovered, with a surface temperature of 200,000 kelvin. This is majorly inappropriate.  Triangulate as less as possible. That movie was amazing. The acting was amazing. The story was amazing.'
-
-proofRead(bigString, ( result ) => {
-  // console.log('send this to the people:', result)
-})
-
-
-setTimeout(() => {
-  console.time('how_long?')
-  proofRead(bigString, ( result ) => {
-    // console.log('send this to the people:', result)
-  })
-  console.timeEnd('how_long?')
-}, 3000)
-
-const cors = microCors({ allowMethods: ['PUT', 'POST'] })
 module.exports = cors(proofReadService)
